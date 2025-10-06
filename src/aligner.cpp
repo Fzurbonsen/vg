8325455b972766e28db2c1a6616af705e52c45f5
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-10-07 00:13:32 +0200

    switched to gwfa

--- src/aligner.cpp
+++ src/aligner.cpp
@@ -1284,31 +1284,31 @@ void Aligner::align_internal(Alignment& alignment, vector<Alignment>* multi_alig
         }
         else {
             // trace back local alignment
-            gssw_graph_mapping* gm = gssw_graph_trace_back (graph,
-                                                            align_sequence->c_str(),
-                                                            align_sequence->size(),
-                                                            nt_table,
-                                                            score_matrix,
-                                                            gap_open,
-                                                            gap_extension,
-                                                            full_length_bonus,
-                                                            full_length_bonus);
+            // gssw_graph_mapping* gm = gssw_graph_trace_back (graph,
+            //                                                 align_sequence->c_str(),
+            //                                                 align_sequence->size(),
+            //                                                 nt_table,
+            //                                                 score_matrix,
+            //                                                 gap_open,
+            //                                                 gap_extension,
+            //                                                 full_length_bonus,
+            //                                                 full_length_bonus);
 
-            // gssw_graph_mapping* gm = gwfa_graph_align_trace_back(graph,
-            //                                                         1,
-            //                                                         max_alt_alns,
-            //                                                         true,
-            //                                                         align_sequence->c_str(),
-            //                                                         nullptr,
-            //                                                         align_sequence->size(),
-            //                                                         nullptr, // should be pinning nodes
-            //                                                         pinning_ids.size(),
-            //                                                         nt_table,
-            //                                                         score_matrix,
-            //                                                         gap_open,
-            //                                                         gap_extension,
-            //                                                         full_length_bonus,
-            //                                                         0);
+            gssw_graph_mapping* gm = gwfa_graph_align_trace_back(graph,
+                                                                    1,
+                                                                    max_alt_alns,
+                                                                    true,
+                                                                    align_sequence->c_str(),
+                                                                    nullptr,
+                                                                    align_sequence->size(),
+                                                                    nullptr, // should be pinning nodes
+                                                                    pinning_ids.size(),
+                                                                    nt_table,
+                                                                    score_matrix,
+                                                                    gap_open,
+                                                                    gap_extension,
+                                                                    full_length_bonus,
+                                                                    0);
         
             gssw_mapping_to_alignment(graph, gm, alignment, pinned, pin_left);
             gssw_graph_mapping_destroy(gm);

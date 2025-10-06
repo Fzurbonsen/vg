66686f44c91597027eca2b6f5814636ccfdd0302
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-10-06 18:27:14 +0200

    updated run_timed_test.py

--- test_scripts/run_timed_test.py
+++ test_scripts/run_timed_test.py
@@ -3,9 +3,14 @@ import sys
 import time
 
 def main():
+
+    print("started: run_timed_test.py\ns")
+
     if len(sys.argv) < 2:
         print("Usage: python time_binary.py <binary> [args...]")
         sys.exit(1)
+
+    print("read inputs\n")
 
     binary = sys.argv[1]
     args = sys.argv[2:]

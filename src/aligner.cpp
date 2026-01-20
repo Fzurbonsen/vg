42969881ce0b9a29035cd80f08dde6daababdf58
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2026-01-20 03:01:34 +0100

    high granularity

--- src/aligner.cpp
+++ src/aligner.cpp
@@ -1343,7 +1343,7 @@ void Aligner::align_internal(Alignment& alignment, vector<Alignment>* multi_alig
 
             call_counter++;
 
-            if (call_counter % 100 == 0) {
+            if (call_counter % 100000 == 0) {
                 fprintf(stderr, "%i ", call_counter);
             }
             

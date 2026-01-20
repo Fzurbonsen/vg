814c74a7a0826fdf1eb2a36e436094fa8956e4c4
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2026-01-20 03:42:42 +0100

    GWFA_CSSWL_INFIX_SIMD version

--- src/aligner.cpp
+++ src/aligner.cpp
@@ -1338,14 +1338,14 @@ void Aligner::align_internal(Alignment& alignment, vector<Alignment>* multi_alig
                                                                     gap_extension,
                                                                     full_length_bonus,
                                                                     0,
-                                                                    GWFA_CSSWL_INFIX,
+                                                                    GWFA_CSSWL_INFIX_SIMD,
                                                                     0);
 
-            call_counter++;
+            // call_counter++;
 
-            if (call_counter % 100000 == 0) {
-                fprintf(stderr, "%i ", call_counter);
-            }
+            // if (call_counter % 100000 == 0) {
+            //     fprintf(stderr, "%i ", call_counter);
+            // }
             
             // debug loop
             // if (gm->cigar.length != gm1->cigar.length) {
